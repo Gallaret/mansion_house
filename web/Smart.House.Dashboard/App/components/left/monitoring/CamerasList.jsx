7b191fafa5213571f9b8fbf8d92b2dd7dd68fb9d
Gallaret <robert.wojda.dev@outlook.com>
2017-07-20 00:30:34 +0200

    Add camera api

--- web/Smart.House.Dashboard/App/components/left/monitoring/CamerasList.jsx
+++ web/Smart.House.Dashboard/App/components/left/monitoring/CamerasList.jsx
@@ -4,18 +4,29 @@ var CamerasList = React.createClass({
     getInitialState: function () {
         return {
             cameraUrl: '',
-            timer: 0
+            timer: 0,
+            name: ''
         };
     },
     componentDidMount: function () {
-        this.stream = setInterval(this.streaming, 50);
+        this.stream = setInterval(this.streaming, 1000);   
     },
     componentWillUnmount: function () {
         clearInterval(this.stream);
     },  
     streaming: function () {
         var image = this.getImage();
+        var name = this.getName();
+     
         this.setState({ cameraUrl: image });
+        this.setState({ name: name });
+    },
+    getName: function () {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "http://localhost:57203/api/camera/1", false);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send();
+        return xhttp.responseText;
     },
     getImage: function () {
         return 'http://192.168.0.234/image/jpeg.cgi?date=' + new Date().toUTCString();
@@ -28,7 +39,7 @@ var CamerasList = React.createClass({
                     <p className="text-center">
                         <img src={this.state.cameraUrl} height="120px" width="160px" />
                     </p>
-                    <label>Salon Front: </label>
+                    <label>{this.state.name}</label>
                 </div>
             </div>
         );          
