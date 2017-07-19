7478585d56474ec16b9b48d3b5a5c6c16f4841be
Gallaret <robert.wojda.dev@outlook.com>
2017-07-19 21:44:37 +0200

    Refactor solution

---
+++ web/Smart.House.Dashboard/App/components/left/monitoring/CamerasList.jsx
@@ -0,0 +1,38 @@
+﻿var React = require('react');
+
+var CamerasList = React.createClass({
+    getInitialState: function () {
+        return {
+            cameraUrl: '',
+            timer: 0
+        };
+    },
+    componentDidMount: function () {
+        this.stream = setInterval(this.streaming, 50);
+    },
+    componentWillUnmount: function () {
+        clearInterval(this.stream);
+    },  
+    streaming: function () {
+        var image = this.getImage();
+        this.setState({ cameraUrl: image });
+    },
+    getImage: function () {
+        return 'http://192.168.0.234/image/jpeg.cgi?date=' + new Date().toUTCString();
+    },
+    render: function () {
+
+        return (
+            <div className="form-inline">
+                <div className="form-group text-center" style={{ margin: '25px' }}>
+                    <p className="text-center">
+                        <img src={this.state.cameraUrl} height="120px" width="160px" />
+                    </p>
+                    <label>Salon Front: </label>
+                </div>
+            </div>
+        );          
+    }
+});
+
+module.exports = CamerasList;
